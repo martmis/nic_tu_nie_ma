a28744edc044bca88635ee86d49585440e7c5866
kunkers <ola.kunowska@gmail.com>
2016-06-10 12:34:02 +0200

    Delete dbconnect.php

--- baza_przyklad/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$host="mysql.agh.edu.pl";		// Nazwa hosta
-	$user="kuna"; 				// Nazwa użytkownika - domyślnie: root
-	$password="CKKv6ouP"; 			// Haslo do bazy
-	$database="kuna";	 	// Nazwa bazy
-	$table="pytania" 			// Nazwa tabeli
-	
-?>
